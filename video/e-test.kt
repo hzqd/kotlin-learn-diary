d1cf7e81690987e96fe518cf39d9e7788413907e
hzqd <40200761+hzqd@users.noreply.github.com>
2018-11-30 14:50:07 +0800

    Add files via upload

---
+++ video/e-test.kt
@@ -0,0 +1,5 @@
+fun main(args: Array<String>) {
+    var son1 = Son()
+    println("儿子的性格：${son1.character}")
+    son1.action()
+}
---
+++ video/e.kt
@@ -0,0 +1,6 @@
+//继承和多态：
+class Son:Father(){
+    override fun action(){
+        println("公共场合有礼貌")
+    }
+}
