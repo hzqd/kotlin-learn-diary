0c00970fd93ffc5c38392fd9bb0897103feac499
hzqd <40200761+hzqd@users.noreply.github.com>
2018-12-02 15:33:46 +0800

    Create 1004成绩排名.kt

---
+++ PTA/PAT 0Basic Level Practice/1004成绩排名.kt
@@ -0,0 +1,54 @@
+/*读入 n（>0）名学生的姓名、学号、成绩，分别输出成绩最高和成绩最低学生的姓名和学号。
+输入格式：
+  每个测试输入包含 1 个测试用例，格式为
+  第 1 行：正整数 n
+  第 2 行：第 1 个学生的姓名 学号 成绩
+  第 3 行：第 2 个学生的姓名 学号 成绩
+    ... ... ...
+  第 n+1 行：第 n 个学生的姓名 学号 成绩
+  其中姓名和学号均为不超过 10 个字符的字符串，成绩为 0 到 100 之间的一个整数，这里保证在一组测试用例中没有两个学生的成绩是相同的。
+输出格式：
+  对每个测试用例输出 2 行，第 1 行是成绩最高学生的姓名和学号，第 2 行是成绩最低学生的姓名和学号，字符串间有 1 空格。
+输入样例：
+3
+Joe Math990112 89
+Mike CS991301 100
+Mary EE990830 95
+输出样例：
+Mike CS991301
+Joe Math990112
+*/
+import java.util.ArrayList
+import java.util.Collections
+import java.util.Scanner
+fun main(args: Array<String>) {
+    val stds = ArrayList<Student>()
+    val input = Scanner(System.`in`)
+    var num = Integer.parseInt(input.nextLine())
+    while (num > 0) {
+        val str = input.nextLine()
+        //System.out.println(str);
+        val temp = str.split(" ".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+        val std = Student()
+        std.name = temp[0]
+        std.stuId = temp[1]
+        std.score = Integer.parseInt(temp[2])
+        stds.add(std)
+        num--
+    }
+    Collections.sort(stds)
+    println(stds[0].name + " " + stds[0].stuId)
+    println(stds[stds.size - 1].name + " " + stds[stds.size - 1].stuId)
+}
+internal class Student : Comparable<Student> {
+    var name: String? = null
+    var stuId: String? = null
+    var score: Int = 0
+    override fun toString(): String {
+        return "Student [name=$name, stuId=$stuId, score=$score]"
+    }
+    override fun compareTo(o: Student): Int {
+        // TODO Auto-generated method stub
+        return -(score - o.score)
+    }
+}
