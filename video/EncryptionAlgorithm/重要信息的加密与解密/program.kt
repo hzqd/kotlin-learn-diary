728c1d4e49a6b198ab11b65139d107300e310c71
hzqd <40200761+hzqd@users.noreply.github.com>
2019-04-07 11:57:19 +0800

    Update and rename a.kt to program.kt

--- video/EncryptionAlgorithm/重要信息的加密与解密/program.kt
+++ video/EncryptionAlgorithm/重要信息的加密与解密/program.kt
@@ -8,20 +8,13 @@ import javax.crypto.spec.SecretKeySpec
 
 object AESCrypt {
     fun encrypt(input: String, passwd: String): String {
-        //1.创建cipher对象：
         val cipher = Cipher.getInstance("AES")
-        //2.初始化cipher：
         cipher.init(Cipher.ENCRYPT_MODE, SecretKeySpec(passwd.toByteArray(), "AES"))
-        //3.加密：
         return Base64.getEncoder().encodeToString(cipher.doFinal(input.toByteArray()))
     }
-
     fun decrypt(input: String, passwd: String): ByteArray {
-        //1.创建cipher对象：
         val cipher = Cipher.getInstance("AES")
-        //2.初始化cipher：
         cipher.init(Cipher.DECRYPT_MODE, SecretKeySpec(passwd.toByteArray(), "AES"))
-        //3.解密：
         return cipher.doFinal(Base64.getDecoder().decode(input))
     }
 }
@@ -31,27 +24,24 @@ fun en() {
     val input = readLine()!!
     println("请您输入至少16位密码:")
     val passwd = readLine()!!
-    val encrypt = AESCrypt.encrypt(input, passwd)
     println("请输入加密后的文件名:")
     val br = BufferedWriter(FileWriter("${readLine()}"))
-    br.write(encrypt)
+    br.write(AESCrypt.encrypt(input, passwd))
     br.close()
-    println("加密完成")
+    println("加密完成\n")
 }
 
 fun de() {
     println("请输入该文件名:")
     val input = BufferedReader(FileReader("${readLine()}")).readLine()
     println("请输入解密密码:")
-    val passwd = readLine()!!
-    println("AES解密：" + String(AESCrypt.decrypt(input, passwd)) + "\n解密完成")
+    println("AES解密：" + String(AESCrypt.decrypt(input, readLine()!!)) + "\n解密完成\n")
 }
 
 fun main() {
     while (true) {
         println("加密请输1  解密请输2")
-        val mode = readLine()
-        when (mode) {
+        when (readLine()) {
             "1" -> en()
             "2" -> de()
         }
